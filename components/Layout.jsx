6284477a3d586b7f713ce9bdbb6d542ad460e38f
sea <boywinterfull@gmail.com>
2024-04-24 17:54:28 +0700

    3

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,24 +4,28 @@ import Slider from "./home/slider";
 
 export default function Layout(props) {
   return (
-    <div>
-      <NavbarDefault />
-      <div className=" h-96">
-        <Slider />
-      </div>
-      <div className=" flex ">
-         <div className=" lg:w-1/6 bg-deep-orange-100  ">
+<div className="flex justify-center">
+<div className="  xl:max-w-screen-xl  ">
+
+<NavbarDefault />
+<div className=" h-[750px]">
+  <Slider />
+</div>
+<div className=" flex  ">
+   <div className=" lg:w-1/6 bg-deep-orange-100  ">
+
+   </div>
+   <div className="  w-full bg-[url('https://th.bing.com/th/id/OIP.In5UvDF83cHj4mAvi8owyQAAAA?rs=1&pid=ImgDetMain')] bg-cover   bg-top   ">
   
-         </div>
-         <div className="  w-full bg-[url('https://th.bing.com/th/id/OIP.In5UvDF83cHj4mAvi8owyQAAAA?rs=1&pid=ImgDetMain')] bg-cover   bg-top   ">
-        
-           <div className="bg-opacity-75 bg-white w-full  h-full">
-           {props.children}
-           </div>
-         
-         </div>
-      </div>
-     
-    </div>
+     <div className="bg-opacity-75 bg-white w-full  h-full">
+     {props.children}
+     </div>
+   
+   </div>
+</div>
+
+</div>
+</div>
+   
   );
 }
